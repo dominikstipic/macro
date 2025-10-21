86578b5018f85fc7add463678bbf4443634e03c2
doms <dominikstipic@gmail.com>
2025-10-21 17:17:54 +0200

    add state values

--- src/test.py
+++ src/test.py
@@ -15,3 +15,20 @@ U = utility_matrix(utility, capital)
 print(U)
 
 #############
+
+# Check the transformation from state index matrix to the capital level matrix with vectorize
+alpha = 0.4
+delta = 0.04
+sigma = 0.5
+beta = 0.96
+epsilon = 0.0001
+
+capital = np.linspace(5, 6, num=3)
+u = UtilityFactory.utility1(alpha, sigma, delta)
+print(f'CAPITAL LEVELS = {capital}')
+state_values, state_path = value_iteration(capital, u, beta, max_time=4)
+
+f = np.vectorize(lambda x: capital[x])
+capital_paths = f(state_path)
+capital_paths
+
