c29dc6050d2b7af04668d65b06f4d587cb0b0da9
Dominik Stipic <dominikstipic@gmail.com>
2025-10-24 17:19:31 +0200

    added comments

--- src/dp.py
+++ src/dp.py
@@ -3,17 +3,33 @@ from typing import List, Dict, Callable, Tuple
 import numpy as np
 import pandas as pd
 
+
+def production(capital: np.array, alpha: float, augmentation=1) -> np.array:
+    """
+        Production function with augmentation. 
+    """
+    return augmentation*(capital**alpha)
+
 def utility(k_now, k_next, alpha, sigma, delta):
-        ct = production(k_now, alpha) + (1-delta)*k_now - k_next
-        #if ct == 0: return np.nan
-        u = (ct**(1-sigma)-1)/(1-sigma)
-        return u
+    """
+        Utility function with numpy
+    """
+    ct = production(k_now, alpha) + (1-delta)*k_now - k_next
+    #if ct == 0: return np.nan
+    u = (ct**(1-sigma)-1)/(1-sigma)
+    return u
 
 def utility_log(k_now, k_next, alpha, delta):
+        """
+           Log utility used in case when sigma = 1. Derived with L'Hospital rule on original utility
+        """
         ct = production(k_now, alpha) + (1-delta)*k_now - k_next
         return np.log(ct)
 
 def utility_matrix(utility, capitals):
+    """
+        Utility for different levels of capital
+    """
     n = len(capitals)
     X = np.zeros((n, n))    
     for i, c1 in enumerate(capitals):
@@ -22,6 +38,9 @@ def utility_matrix(utility, capitals):
     return X
 
 class UtilityFactory:
+    """
+        Build and stores various utilities
+    """
     def utility1(alpha: float, sigma: float, delta: float):
         return lambda k1, k2 : utility(k1, k2, alpha=alpha, sigma=sigma, delta=delta)
 
@@ -34,20 +53,20 @@ class UtilityFactory:
     def utility3(alpha: float, delta: float):
         return lambda k1, k2 : utility_log(k1, k2, alpha, delta)  
 
-def production(capital, alpha, augmentation=1):
-    return augmentation*(capital**alpha)
-
-def consumption(k_now, k_next, alpha, sigma, delta):
-    x = (k_now**alpha) + (1-delta)*k_now - k_next
-    x = x**(1-sigma)
-    return (x-1)/(1-sigma)
-
 
 class DP:
     init_values = np.array([])
     t = 0
 
     def __init__(self, states, utility_function, alpha, beta, capital_deprec, epsilon=0.0001):
+        """
+            states: capital levels or states. 
+            utility_function: function of current and future capital.
+            alpha: production parameter
+            beta: discount factor
+            capital_deprec: capital depreciation
+            epsilon: convergence and float comparison
+        """
         self.states=states
         self.utility_function=utility_function
         self.alpha=alpha
@@ -58,15 +77,25 @@ class DP:
         
 
     def set_init(self, init_states):
+        """Initialize first values of capital"""
         self.init_values = init_states
 
     def bellman(self, utility: Callable[[float, float], float], state_values: Dict[float, float], s0: float, states: List[float], current_capital_stock: list, beta: float) -> Tuple[float, float]:
-        # 0 < k' < f(k)
+        """
+            Bellman iteration with numpy array
+        """
+        # 0 < k' < f(k) - Feasible set
         states[(states > current_capital_stock) | (states < 0)] = np.nan
         values = utility(s0, states) + beta*state_values
         return np.nanargmax(values), np.nanmax(values)
 
+   
+
     def _next(self):
+        """ 
+            Does one value iteration of the DP problem
+            Returns: values, optimal policy indexes, deltas between current and previous iteration
+        """
         new_state_values = np.zeros(len(self.states))
         optim_policy = np.array([])
         for i, s_current in enumerate(self.states):
@@ -78,11 +107,19 @@ class DP:
         self.t += 1
         return new_state_values, optim_policy, deltas
     
-    def next(self):
+    def next(self) -> Tuple[np.array, np.array]:
+        """ 
+            Calls the private _next method. Discard deltas used as convergence criterion. 
+            Returns: values, optimal policy indexes
+        """
         new_state_values, optim_policy, _ = self._next()
         return new_state_values, optim_policy.astype(int)
 
-    def run(self, max_time=1000):
+    def run(self, max_time=1000) -> Tuple[np.array, np.array]:
+        """ 
+            Used for running DP optimization with max time parameter. 
+            Returns: values, optimal policy indexes
+        """
         state_path = np.arange(0, len(self.state_values)).reshape(-1, 1)
         while self.t < max_time:
             self.state_values, optim_policy, deltas = self._next()
@@ -98,6 +135,9 @@ class DP:
         return self.state_values, state_path
 
 def write_csv(ds: dict, csv_name='out.csv'):
+    """
+        Write to dict to csv 
+    """
     ds = {k: [v] for k, v in ds.items()}
     df = pd.DataFrame(ds)
     df.to_csv(csv_name, index=False)
