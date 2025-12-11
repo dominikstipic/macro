0e6ddcae9b3f44fd33babd1dfecabfea72a8c940
Dominik Stipic <dominikstipic@gmail.com>
2025-12-11 22:21:11 +0100

    shooting algorithm

---
+++ src/shooting_algorithm.py
@@ -0,0 +1,87 @@
+import numpy as np
+
+def production(k, alpha):
+    return k**alpha
+
+def inv_product(marginal_product, alpha):
+    return np.pow(marginal_product/alpha, 1/(alpha-1))
+
+def marginal_production(k, alpha):
+    return np.pow(alpha*k, alpha-1)
+
+def utility(c, gamma):
+    return np.pow(c, (1-gamma))/(1-gamma)
+
+def marginal_utility(c, gamma, epsilon=10**-5):
+    return (c+epsilon)**(-gamma)
+
+def capital_steady_state(alpha, beta, delta):
+    ro = (1/beta)-1
+    return np.pow((ro+delta)/alpha, 1/(alpha-1))
+
+def next_capital(produced, delta, capital, consumption, gov_spending):
+    return produced+(1-delta)*capital-consumption-gov_spending
+
+def gross_return(delta, marginal_product):
+    return 1 - delta + marginal_product
+
+def shoot(capital, consumption, gov_spending, alpha, beta, delta, gamma):
+    capital_next = capital**alpha + (1-delta)*capital - consumption - gov_spending
+    if capital_next < 0: capital_next = 0.0001
+    consumption_next = consumption * ((1 + alpha * capital_next**(alpha - 1) - delta) * beta)**(1/gamma)
+    return_t = 1 - delta + np.pow(alpha*capital_next, alpha-1)
+    return consumption_next, capital_next, return_t
+
+def generate_sequence(TS, c0, k0, government_spending, alpha, beta, delta, epsilon=10**-1, CDELTA=0.00001):
+    kss = capital_steady_state(alpha, beta, delta)
+    nan = False
+    k0 = 10
+    for c0 in np.linspace(0.1,0.8, 10000):
+        ct = np.array([c0])
+        kt = np.array([k0])
+        r0 = 1 - delta + marginal_production(k0, alpha)
+        rt = np.array([r0])
+        for _ in range(TS):
+            c, k, r = shoot(kt[-1], ct[-1], government_spending, alpha, beta, delta, gamma)
+            if np.isnan(c) or  np.isnan(k) or np.isnan(r):
+                nan = True
+                break
+            ct = np.append(ct, c)
+            kt = np.append(kt, k)
+            rt = np.append(rt, r)
+
+        if nan:
+            nan = False
+        distance = (kt[-1]-kss)/kss 
+        print(f'c={ct[-1]}, k={kt[-1]}, r={rt[-1]}, distance={distance}')
+        if (abs(distance) <= epsilon): 
+            print('SOLUTION FOUND!')
+            return ct, kt, rt
+        else:
+            if distance > 0:
+                c0 += CDELTA
+            else:
+                c0 -= CDELTA
+    print('SOLUTION NOT FOUND!')
+
+alpha = 0.33
+delta = 0.2
+gamma = 2
+beta = 0.95
+government_spending_old = 0.2 
+government_spending_new = 0.25
+TS = 50
+
+kss = capital_steady_state(alpha, beta, delta) # 1.48
+c0 = production(kss, alpha) - delta*kss - government_spending_old # 0.643
+
+k0 = 10
+c0 = 1.5
+#k_1, c_1, _ = shoot(k0, c0, 0, alpha, beta, delta, gamma)
+
+#print(f"Initial (t=0): k = {k0:.4f}, c = {c0:.4f}")
+#print(f"Next Period (t=1): k = {k_1:.4f}, c = {c_1:.4f}")
+
+print(f'CAPITAL STEADY STATE = {kss}, CONSUMPTION STEADY STATE = {c0}')
+tax = generate_sequence(TS, c0, kss, government_spending_new, alpha, beta, delta)
+
